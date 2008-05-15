09605337fe8151eff7c58d993d21a2b6248d853a
imyousuf <imyousuf@71ddd869-3c4c-0410-9474-71dd1778b0a1>
2008-05-15 04:17:15 +0000

    Add persistent storage read operations, deleted dummy file
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>
    
    
    git-svn-id: https://smart-load-test.googlecode.com/svn/trunk@10 71ddd869-3c4c-0410-9474-71dd1778b0a1

--- smart-load-test/smart-load-test-engine/src/main/java/com/smartitengineering/loadtest/engine/persistence/PersistentTestResultEngine.java
+++ smart-load-test/smart-load-test-engine/src/main/java/com/smartitengineering/loadtest/engine/persistence/PersistentTestResultEngine.java
@@ -19,10 +19,75 @@
  */
 package com.smartitengineering.loadtest.engine.persistence;
 
+import com.smartitengineering.loadtest.engine.result.TestResult;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
 /**
  *
  * @author imyousuf
  */
 public interface PersistentTestResultEngine {
+    
+    /**
+     * Return all the results in this persitent storage
+     * 
+     * @return All stored resutls
+     */
+    public List<TestResult> getAllResults();
 
+    /**
+     * Get all results for the specified test name. Return empty list if no
+     * resutls.
+     * 
+     * @param testName The test name to retrieve
+     * @return List of results, empty list if none
+     */
+    public List<TestResult> getAllForTestName(String testName);
+
+    /**
+     * Retrieve full result for the specified test result id.
+     * 
+     * @param testResultId ID of test result to retrieve
+     * @return test result specified by id; null if no such test result
+     */
+    public TestResult getTestResultById(int testResultId);
+
+    /**
+     * Retrieve all test result between specified date range. All test that
+     * starts greater or equal to startDate or ends lesser or equal to endDate
+     * will be returned in this result.
+     * 
+     * @param startDate Start datetime of the search
+     * @param endDate End datetime of the search
+     * @return All test results within the range
+     */
+    public List<TestResult> getAllResultWithinDateRange(Date startDate,
+                                                        Date endDate);
+
+    /**
+     * Retrieve all test result between specified date range. All test that
+     * starts greater or equal to startDate or ends lesser or equal to endDate
+     * and has the name testName will be returned in this result.
+     * 
+     * @param startDate Start datetime of the search
+     * @param endDate End datetime of the search
+     * @param testName Name of the test
+     * @return All test results within the range and with testName. Empty list
+     * if no such result.
+     */
+    public List<TestResult> getAllResultWithinDateRange(String testName,
+                                                        Date startDate,
+                                                        Date endDate);
+
+    /**
+     * This operation is SPI specific. It intends to provide a strong search
+     * functionality for searching test result. Please consider SPI
+     * documentation to learn more about how to use the filter.
+     * 
+     * @param filters Containing coditions to be used for search
+     * @return The search result; empty list if none.
+     */
+    public List<TestResult> getTestResults(Map<String, ? extends Object> filters);
 }

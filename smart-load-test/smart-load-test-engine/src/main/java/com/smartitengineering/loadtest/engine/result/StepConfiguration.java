e79f6986b9d4b7afc739bfcfecdbe1bcc965e22d
imyousuf <imyousuf@71ddd869-3c4c-0410-9474-71dd1778b0a1>
2008-06-10 06:40:02 +0000

    Improved StepConfiguration property set
    Add step count to distinguish between step configurations. Bring
    clone, equals and hashCode up-to-date with the addition of step
    count.
    
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>
    
    
    git-svn-id: https://smart-load-test.googlecode.com/svn/trunk@70 71ddd869-3c4c-0410-9474-71dd1778b0a1

--- smart-load-test/smart-load-test-engine/src/main/java/com/smartitengineering/loadtest/engine/result/StepConfiguration.java
+++ smart-load-test/smart-load-test-engine/src/main/java/com/smartitengineering/loadtest/engine/result/StepConfiguration.java
@@ -4,15 +4,20 @@
  */
 package com.smartitengineering.loadtest.engine.result;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+
 /**
  * Represents the step configuration that is the delay at a step and number of
  * threads introduced following this thread.
  * 
  * @author imyousuf
  */
+@XmlAccessorType(XmlAccessType.PROPERTY)
 public class StepConfiguration
     implements Cloneable {
 
+    private Integer stepNumber;
     private Integer delayInMillis;
     private Integer stepSize;
 
@@ -33,10 +38,34 @@ public class StepConfiguration
     }
 
     @Override
+    public boolean equals(Object obj) {
+        if(obj != null && obj instanceof StepConfiguration) {
+            return ((StepConfiguration)obj).stepNumber.equals(stepNumber);
+        }
+        else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return stepNumber;
+    }
+    
+    @Override
     public Object clone() {
         StepConfiguration configuration = new StepConfiguration();
         configuration.setStepSize(stepSize);
         configuration.setDelayInMillis(delayInMillis);
+        configuration.setStepNumber(stepNumber);
         return configuration;
+    }
+
+    public Integer getStepNumber() {
+        return stepNumber;
+    }
+
+    public void setStepNumber(Integer stepNumber) {
+        this.stepNumber = stepNumber;
     }
 }

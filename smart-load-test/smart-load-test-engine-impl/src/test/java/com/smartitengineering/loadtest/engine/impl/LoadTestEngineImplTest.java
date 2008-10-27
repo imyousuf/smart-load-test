72de2768e50883ab25fc0f8a22eb77e8c5dc72af
Imran M Yousuf <imyousuf@smartitengineering.com>
2008-10-27 15:18:29 +0600

    Add blank test case for load test engine
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

---
+++ smart-load-test/smart-load-test-engine-impl/src/test/java/com/smartitengineering/loadtest/engine/impl/LoadTestEngineImplTest.java
@@ -0,0 +1,53 @@
+/**
+ *    This module represents an engine IMPL for the load testing framework
+ *    Copyright (C) 2008  Imran M Yousuf (imran@smartitengineering.com)
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.smartitengineering.loadtest.engine.impl;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author imyousuf
+ */
+public class LoadTestEngineImplTest extends TestCase {
+    
+    public LoadTestEngineImplTest(String testName) {
+        super(testName);
+    }            
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testInit() {
+    }
+
+    public void testStart() {
+    }
+
+    public void testGetTestResult() {
+    }
+    
+    public void testAddTestCaseBatchListener() {
+    }
+
+    public void testRemoveTestCaseBatchListener(){
+    }
+
+
+}
